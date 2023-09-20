4cba36c674856f56fa31d2aee0c31b97c27662cd
Favour <gismozfexie@yahoo.com>
2023-09-20 23:28:53 +0100

    chore: update deps + mod few configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#FDF38C', // maize
+        'secondary': '#242529', // raisin black
+        'complementary': '#C0C0C0', // silver
+      },
+      padding: {
+        layout: '60px',
+      },
+      fontFamily: {
+        vastago: ["vastago"],
+      },
+    },
   },
   plugins: [],
 }
