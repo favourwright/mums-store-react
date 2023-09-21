df3685198054f1867339e2283dab23efc2ce381d
Favour <gismozfexie@yahoo.com>
2023-09-21 18:53:33 +0100

    feat: add product card

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,15 @@
+// import { Icon } from '@iconify/react';
+
+interface Props {
+  className?: string
+}
+const ProductCard = ({ className }: Props)=>{
+
+  return (
+    <div className="w-[240px] h-[300px] border border-secondary/10 dark:border-white/10 rounded-3xl">
+      
+    </div>
+  )
+}
+
+export default ProductCard
