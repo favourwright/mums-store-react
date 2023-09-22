80d18b8dc7ed6702f2ff8906c9dcde2b5b6d8711
Favour <gismozfexie@yahoo.com>
2023-09-22 09:58:49 +0100

    chore: move card hover action to pseudo element
    this solved the jittering of cards in containers that have scroll snap
    
    this was the case because I was scalling cards onHover

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -38,39 +38,43 @@ const ProductCard = ({ className, id, image, name, rating=(0.0), price, currency
 
   return (
     <div
-      className={`${className} group w-[250px] h-[320px] flex flex-col gap-3
-      border border-secondary/10 dark:border-white/10 rounded-3xl
-      hover:bg-complementary/30 hover:dark:bg-complementary/10
-      hover:scale-105 hover:border-transparent
-      transition-all duration-300 hover:shadow-2xl relative
-      hover:shadow-black/10 hover:dark:shadow-black/20`}
-      title="APPLE AIRPODS PRO 3">
+      className={`${className} group w-[250px] h-[320px]
+      after:absolute after:inset-0 after:border after:border-secondary/10 after:dark:border-white/10
+      hover:after:border-transparent hover:after:bg-complementary/30 hover:after:dark:bg-complementary/10
+      after:rounded-3xl after:transition-all after:duration-300 hover:after:shadow-2xl relative
+      hover:after:shadow-black/10 hover:dark:after:shadow-black/20 hover:after:scale-105
+      `}
+      title={name}>
       <div
-        className="h-[140px] text-9xl m-1.5 bg-secondary/5 dark:bg-white/5
-        rounded-2xl text-center overflow-hidden relative">
-        <img
-          src={image} alt={name}
-          className="h-full w-full object-center object-cover"
-        />
-        <Link to={`/detail/${id}`} className='absolute inset-0 z-10'></Link>
-      </div>
-
-      <div className="px-5 pb-5 space-y-3 flex-1 flex flex-col justify-between">
-        <div>
-          <div className="truncate opacity-80 group-hover:opacity-100">{name}</div>
-          <div className="opacity-50">{rating} Rating</div>
+        className='w-full h-full relative flex flex-col gap-3 z-10
+        group-hover:scale-105 transition-all duration-300'>
+        <div
+          className="h-[140px] text-9xl m-1.5 bg-secondary/5 dark:bg-white/5
+          rounded-2xl text-center overflow-hidden relative">
+          <img
+            src={image} alt={name}
+            className="h-full w-full object-center object-cover"
+          />
+          <Link to={`/detail/${id}`} className='absolute inset-0 z-10'></Link>
         </div>
 
-        <div className="flex items-center gap-1 justify-between">
-          <div
-            className="font-vastago text-2xl font-semibold
-            text-secondary dark:text-primary
-            opacity-80 group-hover:opacity-100 truncate"
-            title={currency+priceWithDecimals}>
-            {currency}{priceWithDecimals}
+        <div className="px-5 pb-5 space-y-3 flex-1 flex flex-col justify-between">
+          <div>
+            <div className="truncate opacity-80 group-hover:opacity-100">{name}</div>
+            <div className="opacity-50">{rating} Rating</div>
           </div>
-          <div className='flex gap-2'>
-            {secondaryBtns}
+
+          <div className="flex items-center gap-1 justify-between">
+            <div
+              className="font-vastago text-2xl font-semibold
+              text-secondary dark:text-primary
+              opacity-80 group-hover:opacity-100 truncate"
+              title={currency+priceWithDecimals}>
+              {currency}{priceWithDecimals}
+            </div>
+            <div className='flex gap-2'>
+              {secondaryBtns}
+            </div>
           </div>
         </div>
       </div>
