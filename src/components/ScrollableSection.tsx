de491b6dd6e8415b1a8558336168e9c1c72c0335
Favour <gismozfexie@yahoo.com>
2023-09-23 01:57:38 +0100

    chore: minor mod

--- src/components/ScrollableSection.tsx
+++ src/components/ScrollableSection.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import { Icon } from '@iconify/react';
 import useScrollLimitDetector from '../utils/useScrollLimitDetector'
 
